c2b1db2262f2df34e534c421af4e94c69ed8b127
mohak92 <tamhane.mohak@gmail.com>
2021-02-27 22:37:46 -0500

    node implementation

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,79 +1,112 @@
-import React, {Component} from 'react';
-import Node from './Node/Node';
-import {AStar} from '../algorithms/aStar';
+import React, { Component } from "react";
+import Node from "./Node/Node";
+import { AStar } from "../algorithms/aStar";
 
-import './PathfindingVisualizer.css';
+import "./PathfindingVisualizer.css";
 
 export default class PathfindingVisualizer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            grid: [],
-            START_NODE_ROW: 5,
-            FINISH_NODE_ROW: 5,
-            STERT_NODE_COL: 5,
-            FINFISH_NODE_COL: 15,
-            mouseIsPressed: false,
-            ROW_COUNT: 35,
-            COLUMN_COUNT: 35,
-            MOBILE_ROW_COUNT: 10,
-            MOBILE_COLUMN_COUNT: 10,
-            isRunning: false,
-            isStartNode: false,
-            isFinishNode: false,
-            isWallNode: false,
-            currRow: 0,
-            currCol: 0,
-            isDesktopView: true
-        };
-
-        this.handleMouseDown = this.handleMouseDown.bind(this);
-        this.handleMouseLeave = this.handleMouseLeave.bind(this);
-        this.toggleIsRunning = this.toggleIsRunning.bind(this);
-    }
-
-    componentDidMount() {
-        const grid = this.getInitialGrid();
-        this.setState({ grid });
-    }
-
-    toggleIsRunning() {
-        this.setState({ isRunning: !this.state.isRunning });
-    }
-
-    toggleView() {
-        if(!this.state.isRunning) {
-            this.clearGrid();
-            this.clearWalls();
-            const isDesktopView = !this.state.isDesktopView;
-            let grid;
-            if(isDesktopView) {
-                grid = this.getInitialGrid(this.state.ROW_COUNT, this.state.COLUMN_COUNT);
-                this.setState({ isDesktopView, grid });
-            } else {
-                if(this.state.START_NODE_ROW > this.state.MOBILE_ROW_COUNT 
-                    || this.state.FINISH_NODE_ROW > this.state.MOBILE_ROW_COUNT 
-                    || this.state.START_NODE_COL > this.state.MOBILE_COLUMN_COUNT 
-                    || this.state.FINISH_NODE_COL > this.state.MOBILE_COLUMN_COUNT) {
-                        alert('Start & Finish Nodes Must Be within 10 Rows x 20 Columns');
-                } else {
-                    grid = this.getInitialGrid(this.state.MOBILE_ROW_COUNT, this.state.MOBILE_COLUMN_COUNT);
-                    this.setState({isDesktopView, grid});
-                }
-            }
-        }
-    }
-
-    /******************** Set up the initial grid ********************/
-    getInitialGrid = (rowCount = this.state.ROW_COUNT, colCount = this.state.COLUMN_COUNT,) => {
-        const initialGrid = [];
-        for (let row = 0; row < rowCount; row++) {
-            const currentRow = [];
-            for (let col = 0; col < colCount; col++) {
-                currentRow.push(this.createNode(row, col));
-            }
-            initialGrid.push(currentRow);
-        }
-        return initialGrid;
+  constructor() {
+    super();
+    this.state = {
+      grid: [],
+      START_NODE_ROW: 5,
+      FINISH_NODE_ROW: 5,
+      STERT_NODE_COL: 5,
+      FINFISH_NODE_COL: 15,
+      mouseIsPressed: false,
+      ROW_COUNT: 35,
+      COLUMN_COUNT: 35,
+      MOBILE_ROW_COUNT: 10,
+      MOBILE_COLUMN_COUNT: 10,
+      isRunning: false,
+      isStartNode: false,
+      isFinishNode: false,
+      isWallNode: false,
+      currRow: 0,
+      currCol: 0,
+      isDesktopView: true,
     };
+
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.toggleIsRunning = this.toggleIsRunning.bind(this);
+  }
+
+  componentDidMount() {
+    const grid = this.getInitialGrid();
+    this.setState({ grid });
+  }
+
+  toggleIsRunning() {
+    this.setState({ isRunning: !this.state.isRunning });
+  }
+
+  toggleView() {
+    if (!this.state.isRunning) {
+      this.clearGrid();
+      this.clearWalls();
+      const isDesktopView = !this.state.isDesktopView;
+      let grid;
+      if (isDesktopView) {
+        grid = this.getInitialGrid(
+          this.state.ROW_COUNT,
+          this.state.COLUMN_COUNT
+        );
+        this.setState({ isDesktopView, grid });
+      } else {
+        if (
+          this.state.START_NODE_ROW > this.state.MOBILE_ROW_COUNT ||
+          this.state.FINISH_NODE_ROW > this.state.MOBILE_ROW_COUNT ||
+          this.state.START_NODE_COL > this.state.MOBILE_COLUMN_COUNT ||
+          this.state.FINISH_NODE_COL > this.state.MOBILE_COLUMN_COUNT
+        ) {
+          alert("Start & Finish Nodes Must Be within 10 Rows x 20 Columns");
+        } else {
+          grid = this.getInitialGrid(
+            this.state.MOBILE_ROW_COUNT,
+            this.state.MOBILE_COLUMN_COUNT
+          );
+          this.setState({ isDesktopView, grid });
+        }
+      }
+    }
+  }
+
+  /******************** Set up the initial grid ********************/
+  getInitialGrid = (
+    rowCount = this.state.ROW_COUNT,
+    colCount = this.state.COLUMN_COUNT
+  ) => {
+    const initialGrid = [];
+    for (let row = 0; row < rowCount; row++) {
+      const currentRow = [];
+      for (let col = 0; col < colCount; col++) {
+        currentRow.push(this.createNode(row, col));
+      }
+      initialGrid.push(currentRow);
+    }
+    return initialGrid;
+  };
+
+  createNode = (row, col) => {
+    return {
+      row,
+      col,
+      isStart:
+        row === this.state.START_NODE_ROW && col === this.state.START_NODE_COL,
+      isFinish:
+        row === this.state.FINISH_NODE_ROW &&
+        col === this.state.FINISH_NODE_COL,
+      distance: Infinity,
+      distanceToFinishNode:
+        Math.abs(this.state.FINISH_NODE_ROW - row) +
+        Math.abs(this.state.FINISH_NODE_COL - col),
+      isVisited: false,
+      isWall: false,
+      previousNode: null,
+      isNode: true,
+    };
+  };
+
+  /******************** Control mouse events ********************/
 }
